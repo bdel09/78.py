82473c9c59afc66194c1de85bbab74818639d2f9
bdel09 <bmcdelbarrio@gmail.com>
2025-09-01 18:16:44 -0400

    Create student.py

---
+++ student.py
@@ -0,0 +1,9 @@
+#Name: Benjmamin Del Barrio
+#Email: Benjamin.delbarrio31@myhunter.cuny.edu
+
+mess = input("Enter a Phrase:")
+
+for x in mess:
+  print(x, ord(x))
+for i in range(850,1717,17):
+  print(i)
